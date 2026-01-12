e869526cac1e89c60ae0f67ca2f48f91780f9434
Santeri Korpela <sjkorpela03@gmail.com>
2026-01-12 13:47:48 +0200

    Add SpentTiles to track tiles for hand and dora

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import DoraIndicators from "@/app/Components/DoraIndicators";
-
 const tileTypes = ["s1", "s2", "s3", "s4", "s5", "s5r", "s6", "s7", "s8", "s9", "m1", "m2", "m3", "m4", "m5", "m5r", "m6", "m7", "m8", "m9", "p1", "p2", "p3", "p4", "p5", "p5r", "p6", "p7", "p8", "p9", "we", "ws", "ww", "wn", "dg", "dr", "dw", ];
 const maxHandSize = 14;
 const maxDora = 10;
@@ -14,6 +12,7 @@ import Hand from "@/app/Components/Hand";
 import TileButton from "@/app/Components/TileButton";
 import WindSelect from "@/app/Components/WindSelect";
 import Boolean from "@/app/Components/Boolean";
+import DoraIndicators from "@/app/Components/DoraIndicators";
 
 export default function Home() {
 
@@ -48,18 +47,24 @@ export default function Home() {
             allTiles[tile] = 0;
         })
 
-        const a = () => {setHand(allTiles)};
+        const a = () => {setSpentTiles(allTiles); setHand(allTiles);};
         a();
     }, [])
 
     function addTileToHand(tile: string) {
-        if (hand[tile] == undefined) { setHand({...hand, [tile]: 1}) }
-        else if (hand[tile] < maxTileCount) { setHand({...hand, [tile]: hand[tile] + 1}) }
+        if (hand[tile] == undefined) {
+            setHand({...hand, [tile]: 1})
+            setSpentTiles({...spentTiles, [tile]: 1})
+        } else if (hand[tile] < maxTileCount) {
+            setHand({...hand, [tile]: hand[tile] + 1})
+            setSpentTiles({...spentTiles, [tile]: spentTiles[tile] + 1})
+        }
         setHandSize(handSize + 1);
     }
 
     function removeTileFromHand(tile: string) {
         setHand({...hand, [tile]: hand[tile] - 1});
+        setSpentTiles({...spentTiles, [tile]: spentTiles[tile] - 1});
         setHandSize(handSize - 1);
     }
 
@@ -67,10 +72,16 @@ export default function Home() {
         if (doraFull) {
             return;
         }
+        if (spentTiles[tile] == undefined) {
+            setSpentTiles({...spentTiles, [tile]: 1})
+        } else if (hand[tile] < maxTileCount) {
+            setSpentTiles({...spentTiles, [tile]: spentTiles[tile] + 1})
+        }
         setDora([...dora, tile]);
     }
 
     function removeTileFromDora(index: number) {
+        setSpentTiles({...spentTiles, [dora[index]]: spentTiles[dora[index]] - 1});
         setDora(dora => dora.filter((_, i) => (i != index)))
     }
 
@@ -85,7 +96,7 @@ export default function Home() {
             hand: trimmedHand,
             roundWind: roundWind,
             seatWind: seatWind,
-            // doraIndicators: dora,
+            doraIndicators: dora,
             openHand: false,
             flags: {riichi: true}
         }
@@ -93,9 +104,11 @@ export default function Home() {
         console.log("calculationInfo: ", calculationInfo);
     }
 
+    // console.log("spentTiles: ", handSize, dora.length, spentTiles);
+
     return (
         <div className="flex flex-col min-h-screen items-center justify-center gap-3 bg-green-400 min-w-175">
-            <div className={`p-3 bg-green-600 rounded-xl ${handFocus? "outline-white outline-3" : ""}`} onClick={() => setHandFocus(true)}>
+            <div className={`p-3 bg-green-600 rounded-xl ${handFocus? "outline-green-800 outline-5" : ""}`} onClick={() => setHandFocus(true)}>
                 <h1 className="text-xl text-white font-bold text-start">Hand</h1>
                 <Hand hand={hand} removeTileFromHand={removeTileFromHand} maxHandSize={maxHandSize} />
             </div>
@@ -109,9 +122,9 @@ export default function Home() {
                                 // normally there's four copies of a tile,
                                 // but because one five of every land is a red five
                                 // there can only be 3 fives and 1 red five
-                                if (f[2] == 'r') { noMoreTile = hand[f] >= maxRed5TileCount; }
-                                else if (f[1] == '5') { noMoreTile = hand[f] >= max5TileCount; }
-                                else { noMoreTile = hand[f] >= maxTileCount; }
+                                if (f[2] == 'r') { noMoreTile = spentTiles[f] >= maxRed5TileCount; }
+                                else if (f[1] == '5') { noMoreTile = spentTiles[f] >= max5TileCount; }
+                                else { noMoreTile = spentTiles[f] >= maxTileCount; }
                                 return <TileButton
                                     face={f}
                                     addTile={() => handFocus? addTileToHand(f) : addTileToDora(f)}
@@ -121,7 +134,7 @@ export default function Home() {
                             })}
                         </div>
                     </div>
-                    <div className={`p-3 bg-green-600 rounded-xl ${!handFocus? "outline-white outline-3" : ""}`} onClick={() => setHandFocus(false)}>
+                    <div className={`p-3 bg-green-600 rounded-xl ${!handFocus? "outline-green-800 outline-5" : ""}`} onClick={() => setHandFocus(false)}>
                         <h1 className="text-xl text-white font-bold">Dora</h1>
                         <DoraIndicators dora={dora} removeTileFromDora={removeTileFromDora} maxDora={maxDora}/>
                     </div>
