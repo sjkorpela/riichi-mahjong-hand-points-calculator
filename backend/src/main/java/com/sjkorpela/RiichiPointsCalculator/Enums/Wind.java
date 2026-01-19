ce9ee7fb00a30760194dca21a3e60f1c1266b8e4
Santeri Korpela <sjkorpela03@gmail.com>
2026-01-19 15:51:50 +0200

    Fix Git/GitKraken buggign out?

--- backend/src/main/java/com/sjkorpela/RiichiPointsCalculator/Enums/Wind.java
+++ backend/src/main/java/com/sjkorpela/RiichiPointsCalculator/Enums/Wind.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:backend/src/main/java/com/sjkorpela/RiichiPointsCalculator/Enums/Wind.java
 package com.sjkorpela.RiichiPointsCalculator.Enums;
-========
-package com.sjkorpela.RiichiPointsCalculator.enums;
->>>>>>>> origin/backend-dev-1:backend/src/main/java/com/sjkorpela/RiichiPointsCalculator/enums/Wind.java
 
 public enum Wind {
     we,
